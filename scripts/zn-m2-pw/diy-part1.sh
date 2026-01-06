075f482860d7828021d3181a3292ea00a8c961a7
OlaveV <heifu303@gmail.com>
2026-01-06 20:57:42 +0800

    Update diy-part1.sh

--- scripts/zn-m2-pw/diy-part1.sh
+++ scripts/zn-m2-pw/diy-part1.sh
@@ -10,8 +10,15 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#echo 'src-git custom https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
-# 添加 kenzok8 核心源
+# 1. 解锁/添加原有插件源（保留您原脚本的注释示例逻辑）
+#
+# echo 'src-git custom https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+
+# 2. 添加 kenzok8 核心源
+# kenzo: 包含大量常用插件 (如 Passwall, SSR-Plus, OpenClash 等)
+# small: 包含上述插件所需的依赖包
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small.git;master' >> feeds.conf.default
+
+# 3. 确保 feeds.conf.default 存在并具有正确权限
+chmod 644 feeds.conf.default
