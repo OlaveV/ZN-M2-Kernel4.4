fddd0ebec57150525255001d0f01dc3898d7da3e
OlaveV <heifu303@gmail.com>
2026-01-06 20:27:09 +0800

    Update diy-part2.sh

--- scripts/zn-m2-oc/diy-part2.sh
+++ scripts/zn-m2-oc/diy-part2.sh
@@ -9,6 +9,6 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-#优先安装 openclash
+# 优先更新并安装 OpenClash，确保其依赖不会被其他 feed 覆盖
+./scripts/feeds update openclash
 ./scripts/feeds install -a -f -p openclash
-./scripts/feeds install -a -f -p openclash_luci
