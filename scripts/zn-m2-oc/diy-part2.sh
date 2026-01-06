16fb6c3d566b16f47b19cdd50caff51d5dfef48a
OlaveV <heifu303@gmail.com>
2026-01-06 21:33:01 +0800

    Add files via upload

--- scripts/zn-m2-oc/diy-part2.sh
+++ scripts/zn-m2-oc/diy-part2.sh
@@ -9,15 +9,6 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
-# 1. 优先更新并安装 OpenClash，确保其依赖不会被其他 feed 覆盖
-./scripts/feeds update openclash
+#优先安装 openclash
 ./scripts/feeds install -a -f -p openclash
-
-# 2. 优先更新并安装 kenzok8 相关的源
-# 这能确保 TurboACC 等内核级插件使用该仓库的最新代码
-./scripts/feeds update kenzo
-./scripts/feeds install -a -f -p kenzo
-
-./scripts/feeds update small
-./scripts/feeds install -a -f -p small
+./scripts/feeds install -a -f -p openclash_luci
