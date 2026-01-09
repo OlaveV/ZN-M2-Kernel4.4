d5dacb9f822f104baadca55c3f2210aa3f0946e3
OlaveV <heifu303@gmail.com>
2026-01-09 23:21:52 +0800

    Add files via upload

--- scripts/zn-m2-pw/diy-part2.sh
+++ scripts/zn-m2-pw/diy-part2.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
-
-# 1. 替换 Golang 版本为 1.25
-# 建议先完全删除旧包，克隆后再进行 feeds update
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
-
-# 2. 移除源码自带的 dnsmasq 冲突项
-rm -rf feeds/base/package/network/services/dnsmasq
-
-# 3. 强制安装 Passwall 源中的包
-./scripts/feeds update packages
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+#优先安装 passwall 源
 ./scripts/feeds install -a -f -p passwall_packages
-./scripts/feeds install -a -f -p passwall
+./scripts/feeds install -a -f -p passwall_luci
